5479e3cf392d16ca1d31006552147ef7ad37b870
minh tuan <minhtuan2001611@gmail.com>
2021-09-25 21:34:52 +0700

    them file index.js , them function cho file Dog

--- Dog.js
+++ Dog.js
@@ -1,7 +1,11 @@
 function Dog(){
+	this.name = name ;
 	this.somach = [];
 }
 Dog.prototype.eat = function(cat){
 	this.stomach.push(cat);
 }
-	
+Dog.prototype.sayHi = function(first_argument){
+	console.log(" I am Dog . Hello " + first_argument);
+}
+module.export = Dog;
---
+++ index.js
@@ -0,0 +1,4 @@
+var Dog = require('Dog') ;
+
+var Dog2 = require('nobita');
+dog.sayHi();
