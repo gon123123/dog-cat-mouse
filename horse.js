a264a84cac177b6aa564777493ed88baaedba20a
minh tuan <minhtuan2001611@gmail.com>
2021-09-27 06:58:51 +0700

    Horse run method

--- horse.js
+++ horse.js
@@ -3,4 +3,7 @@ function Horse(){
 Horse.prototype.jump = function (argument) {
 	console.log("jump .....");
 }
+Horse.prototype.run = function(){
+	console.log('Running');
+};
 module.export = Horse;
