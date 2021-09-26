3361aa437e69b6a2f7a4fbd09ed7d1437bd0a1d0
minh tuan <minhtuan2001611@gmail.com>
2021-09-27 05:59:11 +0700

    Improve cat method

--- cat.js
+++ cat.js
@@ -1,5 +1,12 @@
+var Mouse = require('mouse');
 function cat(){
 	this.stomach = [];
 	console.log("hi caho chuot");
 }
+Cat.prototype.eat = function(animal){
+	if(animal instanceof Mouse){
+		this.stomach.push(animal);
+	} else{
+		throw new Error('cat can only eat mouse !');
+};
 module.export = Cat;
